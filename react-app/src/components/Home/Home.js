80647599e7e451f18a56a271e54c86c448ca7f8c
Nick <nikhilmenon@comcast.net>
2021-02-04 16:03:49 -0600

    video prop good

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import { setLoginModal, setSignupModal } from '../../store/modal'
 import { useDispatch } from 'react-redux'
+import sample from './TrueClip.mp4';
+import "./Home.css"
+
 
 function Home() {
 
     return (
         <div>
-            hello
+            <div id="videodiv">
+            <video autoPlay loop muted playsInline>
+              <source type="video/mp4" src={sample} />
+            </video>
+            </div>
         </div>
     )
 }
