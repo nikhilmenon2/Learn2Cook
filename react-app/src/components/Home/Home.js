2ab764ac685f2481f6bb0fceb97cf3c50a9aeaf0
Nick <nikhilmenon@comcast.net>
2021-02-08 15:28:19 -0600

    Working Carasouel in Home Page

--- react-app/src/components/Home/Home.js
+++ react-app/src/components/Home/Home.js
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from "react";
 import sample from './TrueClip.mp4';
 import "./Home.css"
-import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import { homeRecipesDisplay } from "../../store/recipes";
 import { useDispatch, useSelector } from "react-redux";
-
+import Carousel from "react-elastic-carousel";
 function Home() {
 
   const [recipes, setRecipes] = useState([]);
@@ -22,22 +21,21 @@ function Home() {
         return null;
       }
 
-
-     const recipeComponents = recipearray.map((recipe) => {
-        return (
-          <a href={`/recipes/${recipe.id}`}>
-            <div className="individual-box" key={recipe.id}>
-              <div className="recipes-picture-box">
-                <img className="recipes-picture" src={recipe.image}></img>
-              </div>
-              <div className="recipes-information-box">
-                <h3>{recipe.title}</h3>
-                <h5>Time: {recipe.time} Minutes</h5>
-              </div>
-            </div>
-          </a>
-        );
-      });
+  const recipeComponents = recipearray.map((recipe) => {
+    return (
+      <a href={`/recipes/${recipe.id}`}>
+        <div className="individual-box" key={recipe.id}>
+          <div className="recipes-picture-box">
+            <img className="recipes-picture" src={recipe.image}></img>
+          </div>
+          <div className="recipes-information-box">
+            <h3>{recipe.title}</h3>
+            <h5>Time: {recipe.time} Minutes</h5>
+          </div>
+        </div>
+      </a>
+    );
+  });
 
  
 
@@ -48,7 +46,8 @@ function Home() {
             <source type="video/mp4" src={sample} />
           </video>
         </div>
-        <div className="all-recipe-box">{recipeComponents}</div>
+        <h1>Check out some Vegeterian Options</h1>
+        <Carousel itemsToShow={5}>{recipeComponents}</Carousel>
       </div>
     );
 }
