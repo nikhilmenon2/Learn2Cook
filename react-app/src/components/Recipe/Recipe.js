df4d7324238ba52067e154f19206f2240a950b0e
Nick <nikhilmenon@comcast.net>
2021-02-09 18:22:36 -0600

    styling changes

--- react-app/src/components/Recipe/Recipe.js
+++ react-app/src/components/Recipe/Recipe.js
@@ -72,57 +72,61 @@ function Recipe() {
   const next = recipe.recipeId + 1
 
   return (
-    <div className="recipe">
-      <div className="grid-wrap">
-        <article>
-          <h1 id="article-title">{recipe.title}</h1>
-          <div id="author-div">
-            <a id="author-link" href={`/users/${recipe.userId}`}>
-              <h3 id="author">
-                By {authors.firstName} {authors.lastName}
-              </h3>
-            </a>
-          </div>
-          <div id = "favorite-parent">
-            <div id="under-author">
-              <h3 className="time">
-                <span class="bigger">Yield</span> {recipe.servings} Servings
-              </h3>
-              <h3 className="time">
-                <span class="bigger">Time</span> {recipe.time} Minutes
-              </h3>
+    <div id="parent-recipe-div">
+      <div className="side-gold"></div>
+      <div className="recipe">
+        <div className="grid-wrap">
+          <article>
+            <h1 id="article-title">{recipe.title}</h1>
+            <div id="author-div">
+              <a id="author-link" href={`/users/${recipe.userId}`}>
+                <h3 id="author">
+                  By {authors.firstName} {authors.lastName}
+                </h3>
+              </a>
             </div>
-            <Favorite recipeId={recipeId} user={user} />
-          </div>
-          <div id="picture-container">
-            <p id="article-description">{recipe.description}</p>
-            <img id="recipe-picture" src={recipe.image}></img>
-          </div>
-          <div id="list-container">
-            <div id="ingredient-container">
-              <h3 className="heading-title">Ingredients</h3>
-              {ingredients.map((ingredient) => (
-                <div id="ingredient-list">
-                  {ingredient.amount} {ingredient.unit}{" "}
-                  {ingredient.ingredientName}{" "}
-                </div>
-              ))}
+            <div id="favorite-parent">
+              <div id="under-author">
+                <h3 className="time">
+                  <span class="bigger">Yield</span> {recipe.servings} Servings
+                </h3>
+                <h3 className="time">
+                  <span class="bigger">Time</span> {recipe.time} Minutes
+                </h3>
+              </div>
+              <Favorite recipeId={recipeId} user={user} />
             </div>
-            <div id="instruction-container">
-              <h3 className="heading-title">Preparation</h3>
-              <div id="instructions-list">{instructionscomp}</div>
+            <div id="picture-container">
+              <p id="article-description">{recipe.description}</p>
+              <img id="recipe-picture" src={recipe.image}></img>
             </div>
-          </div>
-          <div id="review-section">
-            <div id="filler-div"></div>
-            <div>
-              <h3 className="heading-title">Reviews</h3>
-              <WriteReview recipeId={recipeId} user={user} />
-              <div id="review-parent-div">{reviewcomp}</div>
+            <div id="list-container">
+              <div id="ingredient-container">
+                <h3 className="heading-title">Ingredients</h3>
+                {ingredients.map((ingredient) => (
+                  <div id="ingredient-list">
+                    {ingredient.amount} {ingredient.unit}{" "}
+                    {ingredient.ingredientName}{" "}
+                  </div>
+                ))}
+              </div>
+              <div id="instruction-container">
+                <h3 className="heading-title">Preparation</h3>
+                <div id="instructions-list">{instructionscomp}</div>
+              </div>
             </div>
-          </div>
-        </article>
+            <div id="review-section">
+              <div id="filler-div"></div>
+              <div>
+                <h3 className="heading-title">Reviews</h3>
+                <WriteReview recipeId={recipeId} user={user} />
+                <div id="review-parent-div">{reviewcomp}</div>
+              </div>
+            </div>
+          </article>
+        </div>
       </div>
+      <div className="side-gold"></div>
     </div>
   );
 }
